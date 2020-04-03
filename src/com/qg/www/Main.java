0de4683f136cb22027318d4b49145b5b01b0dbc8
huange7 <1048461654@qq.com>
2020-04-03 10:09:15 +0800

    测试提交

---
+++ src/com/qg/www/Main.java
@@ -0,0 +1,14 @@
+package com.qg.www;
+
+/**
+ * @ClassName Main
+ * @Description 主函数
+ * @Author huange7
+ * @Date 2020-04-03 10:08
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
